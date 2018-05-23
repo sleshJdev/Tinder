8106c5cb251ca98a843b294b36a189fada7fb12c
Eugene <slesh.eugene93@gmail.com>
2018-05-24 00:12:04 +0300

    hide profile screen temporally

--- App/components/global-widgets/nav.js
+++ App/components/global-widgets/nav.js
@@ -6,16 +6,12 @@
 
 import React, { Component } from 'react';
 import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
-import Iconz from 'react-native-vector-icons/Ionicons';
 
 export default class Nav extends Component {
 
   home() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.props.toProfile}>
-          <Iconz name="ios-person" color="#888" size={25} style={{ margin: 10 }}/>
-        </TouchableOpacity>
         <Image source={require('../../images/logo.png')} resizeMode="contain"
                style={{ width: 100, height: 30 }}/>
       </View>
@@ -25,42 +21,18 @@ export default class Nav extends Component {
   review() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.props.toProfile}>
-          <Iconz name="ios-person" color="#888" size={25} style={{ margin: 10 }}/>
-        </TouchableOpacity>
         <TouchableOpacity onPress={this.props.toHome}>
           <Image source={require('../../images/tinder.png')}
                  resizeMode="contain"
                  style={{ width: 100, height: 30 }}/>
         </TouchableOpacity>
-        <TouchableOpacity onPress={this.props.toHome}>
-          <Image source={require('../../images/logo.png')}
-                 style={{ width: 100, height: 30, margin: 10 }}/>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  profile() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.props.toHome}>
-          <Image source={require('../../images/tinder.png')}
-                 resizeMode="contain"
-                 style={{ width: 100, height: 30 }}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.props.toHome}>
-          <Image source={require('../../images/logo.png')}
-                 style={{ width: 100, height: 30, margin: 10 }}/>
-        </TouchableOpacity>
+        <Image source={require('../../images/logo.png')} resizeMode="contain"
+               style={{ width: 100, height: 30, margin: 10 }}/>
       </View>
     );
   }
 
   render() {
-    if (this.props.type === 'profile') {
-      return this.profile();
-    }
     if (this.props.type === 'review') {
       return this.review();
     }
